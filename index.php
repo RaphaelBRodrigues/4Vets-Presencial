a82f67a7801f02fb7503f2da7e8f9bccb8e861ac
EduduNunu <eduardoluisdoliveira@gmail.com>
2020-03-11 17:23:08 -0300

    Index responsivo e finalizado

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<link rel="shortcut icon" href="https://i.ya-webdesign.com/images/transparent-favicons-books-11.png">
+<meta name="theme-color" content="#95b5a6">
+
+
+    <link rel="stylesheet" href="View/css/index.css">
+    <title>4Vets</title>
+</head>
+<body>
+
+</body>
+</html>
